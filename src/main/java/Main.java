b05078aab4cc2b4fd7f6492740fc7cec0ffc1a6e
tautvydast3 <42258727+tautvydast3@users.noreply.github.com>
2021-06-18 12:50:18 +0300

    beveik baigtas frontendas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,116 +1,64 @@
+
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        // JFrame = GUI window to add components to
-        JFrame frame = new JFrame(); // creates a frame
-        frame.setTitle("Horoskopas"); //sets title of frame
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit of of application
-        frame.setResizable(true); //prevents frame from resizing
-        frame.setSize(440,500); //set the x-dimension and y-dimension of frame
-        frame.setVisible(true); // make frame visible
+        int width = 420;
+        int height = 420;
+
+        MyFrame frame = new MyFrame(width, height, "Trispalve");
+        frame.setSize(height, width);
+       // frame.setLayout(new GridLayout(3, 1, 0, 0));
 
         ImageIcon logo = new ImageIcon("src/main/zodiak-logo.png"); //create an ImageIcon
         frame.setIconImage(logo.getImage()); //change icon of frame
 
-        frame.getContentPane().setBackground(Color.decode("#f4f4f4"));
+        // sukurti JPanel header
+        JPanel header = new JPanel();
+        header.setBackground(Color.decode("#556677"));
+        header.setSize(width, height / 10);
+        frame.add(header, BorderLayout.PAGE_START);
 
-       // MyFrame myFrame = new MyFrame();
+        // sukurti JPanel body
+        JPanel body = new JPanel();
+        body.setBackground(Color.decode("#445566"));
+        body.setSize(width, height/3);
+        frame.add(body, BorderLayout.CENTER);
 
-        JLabel labelAries = new JLabel(); // creates a label
-        JLabel labelTaurus = new JLabel();
-        JLabel labelGemini = new JLabel();
-        JLabel labelCancer = new JLabel();
-        JLabel labelLeo = new JLabel();
-        JLabel labelVirgo = new JLabel();
-        JLabel labelLibra = new JLabel();
-        JLabel labelScorpio = new JLabel();
-        JLabel labelSagittarius = new JLabel();
-        JLabel labelCapricorn = new JLabel();
-        JLabel labelAquarius = new JLabel();
-        JLabel labelPisces = new JLabel();
+        // sukurti JPanel footer
+        JPanel footer = new JPanel();
+        footer.setBackground(Color.decode("#335566"));
+        footer.setSize(width,height/3);
+        frame.add(footer, BorderLayout.PAGE_END);
 
-        labelAries.setText("Avinas\n" +
-                "03.21 - 04.20\n" +
-                "Nesistenkite visko tempti vieni, jeigu užsiimate išsamiu namų, sodybos ar darbo vietos tvarkymu. Dalinkitės pareigomis su šalia esančiais, tarkitės dėl išlaidų. Nenumokite ranka į sveikatos sutrikimus."); // set text of label1
-        labelTaurus.setText("Jautis\n" +
-                "04.21 - 05.21\n" +
-                "Tikriausiai šiandien patirsite ypatingų emocinių impulsų. Galite leistis į lengvabūdišką avantiūrą, ką nors vilioti, flirtuoti. Turėtų pagerėti santykiai su vaikais.");
-        labelGemini.setText("Dvyniai\n" +
-                "05.22 - 06.21\n" +
-                "Trokšite geresnių sąlygų, komforto, patogumų. Galbūt kažką pertvarkinėsite. Tai gali kainuoti daugiau, nei apskaičiavote iš anksto. Pasisaugokite sukčių.");
-        labelCancer.setText("\n" +
-                "Vėžys\n" +
-                "06.22 - 07.22\n" +
-                "Savo energiją nukreipkite į darbą, mokslą ar kūrybą, o ne į tuščius ginčus, ambicijų demonstravimą. Nuo to priklausys, kaip pakryps pokalbis, turėsiantis įtakos jūsų autoritetui.");
-        labelLeo.setText("Liūtas\n" +
-                "07.23 - 08.23\n" +
-                "Aiškės finansinės perspektyvos, galėsite numatyti, kaip racionaliau panaudoti turimas lėšas. Regis, jums kils ūpas šį bei tą nusipirkti, atsinaujinti. O galbūt rūpinsitės dovana kažkam, kas jums išties rūpi.");
-        labelVirgo.setText("Mergelė\n" +
-                "08.24 - 09.23\n" +
-                "Tai gali būti naujų galimybių diena, kai po ilgų pastangų ims ryškėti perspektyvos. Viskas taip trapu, kad lengva sugadinti. Saugokitės klastų, vagysčių, apsinuodijimo.");
-        labelLibra.setText("Svarstyklės\n" +
-                "09.24 - 10.23\n" +
-                "Gana neutrali diena – kaip pasiklosite, taip išsimiegosite. Nelabai tiktų imtis intensyvios veiklos. Pasisaugokite, kad dėl alkoholio ar netinkamų produktų, vaistų vartojimo nekiltų problemų.");
-        labelScorpio.setText("Skorpionas\n" +
-                "10.24 - 11.22\n" +
-                "Šiandien teigiamai veiksite aplinkinius ir jie – jus. Žadinsite draugiškumo bei solidarumo jausmus. Verta pamėginti atnaujinti jums svarbius santykius, jeigu jie neseniai patyrė krizę.");
-        labelSagittarius.setText("Šaulys\n" +
-                "11.23 - 12.22\n" +
-                "Jeigu būtina, šiandien tvarkykite dokumentus, kreipkitės į valdžią dėl leidimų, oficialių raštų, patvirtinimų, atsiskaitymų. Bus nelengva, tačiau naudokitės tomis progomis, kurios įmanomos.");
-        labelCapricorn.setText("Ožiaragis\n" +
-                "12.23 - 01.20\n" +
-                "Šiandien ypač aktualūs gali būti su išsilavinimu, studijomis, įstatymais, kelionėmis susiję reikalai. Galbūt nustebins kolega ar pažįstamas iš užsienio.");
-        labelAquarius.setText("Vandenis\n" +
-                "01.21 - 02.19\n" +
-                "Šiandien ne viskas džiugins. Galbūt kažkas primins negrąžintą skolą, paprašys paslaugos, kurią gana keblu suteikti. Galite įsigyti brokuotą daiktą, dėl jo aiškintis ir pan.");
-        labelPisces.setText("Žuvys\n" +
-                "02.20 - 03.20\n" +
-                "Jūsų nuotaikos ir emocijos bus labai nepastovios. Neleiskite, kad sąmonę užvaldytų nepagrįsti įtarimai, pavydas. Tai prasti palydovai, norint išsaugoti santykius.");
+        //frame.setVisible(true);
 
-   //     label1.setHorizontalTextPosition(JLabel.RIGHT); // set text LEFT, RIGHT, or CENTER of image
-
-        JTextField textField1 = new JTextField("Year",20);
-        JTextField textField2 = new JTextField("Month",20);
-        JTextField textField3 = new JTextField("Day",20);
-        JTextField textField4 = new JTextField("4 - asis laukelis");
-
-        textField1.setSize(200,50);
-        textField1.setHorizontalAlignment(JTextField.CENTER);
-        textField1.setVisible(true);
-        textField2.setSize(200,50);
-        textField2.setHorizontalAlignment(JTextField.CENTER);
-        textField2.setVisible(true);
-        textField3.setSize(200,50);
-        textField3.setHorizontalAlignment(JTextField.CENTER);
-        textField3.setVisible(true);
-        textField4.setSize(0,0);
-        textField4.setVisible(true);
-        textField4.setHorizontalAlignment(JTextField.CENTER);
-
-        textField1.setBounds(10,10,100,20);
-        textField2.setBounds(160,10,100,20);
-        textField3.setBounds(310,10,100,20);
-        textField4.setBounds(0,0,0,0);
-       // textField4.setBounds(320,10,100,20);
-
-//        myFrame.add(textField1);
-        frame.add(textField1);
-//        myFrame.add(textField2);
-        frame.add(textField2);
-//        myFrame.add(textField3);
-        frame.add(textField3);
-//        myFrame.add(textField4);
-        frame.add(textField4);
-
-//        myFrame.add(label1);
-//        frame.add(label1);
+        //sukuriu input boxus
+        JTextField inpBox1 = new JTextField("Year", 10);
+        JTextField inpBox2 = new JTextField("Month", 10);
+        JTextField inpBox3 = new JTextField("Day", 10);
 
 
+        // input boxus pridedu prie header paneles
 
+        header.add(inpBox1);
+        header.add(inpBox2);
+        header.add(inpBox3);
 
+        //Sukuriu mygtukus
+
+        JButton btnFind = new JButton("Find");
+        JButton btnClear = new JButton("Clear");
+        btnFind.setSize(width/3, height/10);
+        btnClear.setSize(width/3, height/10);
+        // mygtukus pridedu prie footer paneles
+
+        footer.add(btnFind);
+        footer.add(btnClear);
+
+        frame.setVisible(true);
     }
 }
--- src/main/java/MyFrame.java
+++ src/main/java/MyFrame.java
@@ -1,21 +1,23 @@
-//import javax.swing.*;
-//import java.awt.*;
-//
-//public class MyFrame extends JFrame {
-//    MyFrame() {
-//        this.setTitle("Title goes here"); //sets title of frame
-//        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit of of application
-//        this.setResizable(true); //prevents frame from resizing
-//        this.setSize(420, 500); //set the x-dimension and y-dimension of frame
-//        this.setVisible(false); // make frame visible
-//
-//        ImageIcon logo = new ImageIcon("src/main/zodiac-logo.png"); //create an ImageIcon
-//        this.setIconImage(logo.getImage()); //change icon of frame
-//
-//
-//
-//        this.getContentPane().setBackground(Color.decode("#f4f4f4"));
-//
-//    }
-//
-//}
+import javax.swing.*;
+import java.awt.*;
+
+public class MyFrame extends JFrame {
+    MyFrame() {
+        this.setTitle("Title goes here"); //sets title of frame
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //exit of of application
+        this.setResizable(false); //prevents frame from resizing
+        this.setSize(420, 500); //set the x-dimension and y-dimension of frame
+        this.setVisible(true); // make frame visible
+
+        ImageIcon logo = new ImageIcon("src/main/zodiac-logo.png"); //create an ImageIcon
+        this.setIconImage(logo.getImage()); //change icon of frame
+
+
+
+        this.getContentPane().setBackground(Color.decode("#335566"));
+
+    }
+
+    public MyFrame(int width, int height, String trispalve) {
+    }
+}
